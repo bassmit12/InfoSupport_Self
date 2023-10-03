b951ca86cee32d4a3d05111df71cfa57a18d3918
Bas Smit <bas.smit@live.nl>
2023-10-03 13:34:48 +0200

    Added infoPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import FrontPage from "./pages/FrontPage";
 import Home from "./pages/Home";
+import ItemDetails from "./pages/ItemDetails";
 
 function App() {
   return (
     <div>
       <FrontPage />
       <Home />
+      <ItemDetails />
     </div>
   );
 }
