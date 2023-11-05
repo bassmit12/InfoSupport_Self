30c8242246d69b8c10c6b2eb5e16f29b24006fbc
Bas Smit <bas.smit@live.nl>
2023-11-05 17:20:42 +0100

    Update ItemDetails.tsx

--- client/src/pages/Users/ItemDetails.tsx
+++ client/src/pages/Users/ItemDetails.tsx
@@ -62,37 +62,50 @@ const ItemDetails = () => {
   return (
     <>
       <Header />
-      <section className="my-12 max-w-screen-xl mx-auto px-6">
-        <div className="flex items-center justify-center">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-20 mt-12">
-            <div className="flex-col flex-1">
-              <h1 className="text-5xl mb-9 font-semibold">{foodItem.name}</h1>
-              <p className="text-xl font-light">{foodItem.descriptionLong}</p>
-              <div className="mt-10 flex flex-row ">
-                <h2 className="text-gray-900 poppins text-2xl font-semibold">
+      <section className="max-w-screen-xl mx-auto px-6">
+        <div className="flex flex-col justify-center items-center h-screen">
+          <div
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10"
+            key={foodItem._id}
+          >
+            <div className="order-2 md:order-1 lg:order-1 flex flex-col justify-center">
+              <h1 className="text-center md:text-left lg:text-left text-3xl lg:text-4xl font-semibold poppins pb-4 text-gray-700 select-none">
+                {foodItem.name}
+              </h1>
+              <p className="text-center md:text-left lg:text-left text-sm poppins text-gray-500 leading-relaxed select-none">
+                {foodItem.descriptionLong}
+              </p>
+              <div className="flex items-center justify-center md:justify-start lg:justify-start space-x-6 pt-8">
+                <h2 className="text-3xl font-bold text-black poppins select-none">
                   ${foodItem.price.toFixed(2)}
                 </h2>
                 <Plus_Min_Button />
               </div>
               <Link to="/TransactionPage">
-                <button className="bg-primary text-white px-8 py-3 focus:outline-none poppins rounded-full mt-8 transform transition duration-300 hover:scale-105 flex flex-row">
-                  <div className="mr-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="24"
-                      viewBox="0 -960 960 960"
-                      width="24"
-                      fill="#ffffff"
-                    >
-                      <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
-                    </svg>
-                  </div>
-                  <h1>Order now</h1>
-                </button>
+                <div className="mt-8 flex items-center justify-center md:justify-start lg:justify-start">
+                  <button className="bg-primary text-white px-8 py-3 focus:outline-none poppins rounded-full mt-8 transform transition duration-300 hover:scale-105 flex flex-row">
+                    <div className="mr-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24"
+                        viewBox="0 -960 960 960"
+                        width="24"
+                        fill="#ffffff"
+                      >
+                        <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
+                      </svg>
+                    </div>
+                    <h1>Order now</h1>
+                  </button>
+                </div>
               </Link>
             </div>
-            <div className="w-full h-auto justify-center my-auto">
-              <img src={foodItem.imageURL} alt={foodItem.name} />
+            <div className="order-1 md:order-2 lg:order-2">
+              <img
+                src={foodItem.imageURL}
+                alt={foodItem.name}
+                className="w-3/4 md:w-3/4 lg:w-full mx-auto"
+              />
             </div>
           </div>
         </div>
