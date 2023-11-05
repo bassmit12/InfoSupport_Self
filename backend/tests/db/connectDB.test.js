8cf173d8217839248467e460c28e33a1b82a0235
Bas Smit <bas.smit@live.nl>
2023-11-05 14:44:13 +0100

    wrote some unit tests

---
+++ backend/tests/db/connectDB.test.js
@@ -0,0 +1,30 @@
+// test/db/connectDB.test.js
+const mongoose = require("mongoose");
+
+describe("MongoDB Memory Server", () => {
+  test("should connect using MONGO_URI from environment", async () => {
+    const uri = process.env.MONGO_URI;
+    expect(uri).toBeDefined();
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    // Check if the connection is active
+    const connectionState = mongoose.connection.readyState;
+    expect(connectionState).toBe(1); // 1 means connected
+
+    // Get a reference to the test collection
+    const testCollection = mongoose.connection.collection("test");
+
+    // Insert a document and check the result
+    const insertResult = await testCollection.insertOne({ test: "data" });
+    expect(insertResult.acknowledged).toBeTruthy();
+    expect(insertResult.insertedId).toBeDefined();
+
+    // Clean up after test
+    await mongoose.connection.dropDatabase();
+    await mongoose.disconnect();
+  });
+});
--- backend/tests/jest.setup.js
+++ backend/tests/jest.setup.js
@@ -1,10 +1,10 @@
-// jest.setup.js
 const { MongoMemoryServer } = require("mongodb-memory-server");
 
 let mongoServer;
 
 beforeAll(async () => {
   mongoServer = new MongoMemoryServer();
+  await mongoServer.start(); // Start the server
   const mongoUri = await mongoServer.getUri();
   process.env.MONGO_URI = mongoUri; // Set the MONGO_URI to the in-memory database URI
 });
