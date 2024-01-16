a691fb61a02d4bbe6c8d5e0d44180c96886ee7d6
Bas Smit <bas.smit@live.nl>
2024-01-16 16:12:30 +0100

    removed sockets

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,9 @@ import cartRoutes from "./routes/cartRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
-import http from "http"; // Import the 'http' module
-import { Server } from "socket.io"; // Import the 'Server' class
-
 dotenv.config();
 connectDB();
 const app = express();
-
-const server = http.createServer(app); // Create an HTTP server
-
-const io = new Server(server); // Assuming you have httpServer created
-
-app.set("socketio", io); // Set the io instance as part of the app for access in controllers
-
-export { io }; // Create a new instance of the Socket.IO server
 
 const PORT = process.env.PORT || 5002;
 
@@ -40,21 +29,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
 );
-
-app.get("/ping", (req, res) => {
-  res.send("pong 🏓");
-});
-
-// Add a WebSocket connection event
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  // Example: Listen for a stock update event and broadcast it to all clients
-  socket.on("stockUpdate", (data) => {
-    io.emit("stockUpdate", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
