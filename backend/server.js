c5d5378be937f51edcf4df0fbf388ed320eb6c0c
Bas Smit <bas.smit@live.nl>
2024-01-16 16:49:44 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import foodRoutes from "./routes/foodRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,12 @@ const PORT = process.env.PORT || 5002;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Adjust the origin to match your React app
+    credentials: true,
+  })
+);
 
 app.use("/api/food", foodRoutes);
 app.use("/api/cart", cartRoutes);
