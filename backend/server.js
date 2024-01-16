f9e268e84dd256960971b04a9970a1979a93345f
Bas Smit <bas.smit@live.nl>
2024-01-16 16:07:31 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import foodRoutes from "./routes/foodRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import cors from "cors";
+
 import http from "http"; // Import the 'http' module
 import { Server } from "socket.io"; // Import the 'Server' class
 
@@ -27,7 +27,6 @@ const PORT = process.env.PORT || 5002;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
 
 app.use("/api/food", foodRoutes);
 app.use("/api/cart", cartRoutes);
