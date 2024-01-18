14126caa947fef14a808c86c36210c79b9a2b987
Bas Smit <bas.smit@live.nl>
2024-01-18 13:31:55 +0100

    Update protectRoute.js

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -7,7 +7,7 @@ const protectRoute = async (req, res, next) => {
 
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, "jwt");
 
     const user = await User.findById(decoded.userId).select("-password");
     req.user = user;
